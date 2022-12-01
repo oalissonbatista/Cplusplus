fad1b204318c270dcd7a7df5a76df0b81b7e4101
oalissonbatista <alisson252525@hotmail.com>
2022-12-01 19:47:46 -0300

    Exercicios B da lista 3

--- Programas/B3.cpp
+++ Programas/B3.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
     int cont =0;
     string frase;
-    cout << "Digite: ";
     cin >> frase;
-    //palavra: alisson
     for (int i=0; i <= frase.length(); i++){
         for(int j = 1; j <= frase.length(); j++){
             if(frase[i] == frase[j]){
                 cont ++;
-                
             }
         }
     }
-    cout << cont;
-    
+    if (cont > frase.length()){
+        cout << "no\n"; 
+    } else {
+        cout << "yes\n";
+    }
     return 0;
 }
