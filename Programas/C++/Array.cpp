4f8afe808037c0c58cfc788b0a3c6c2f3e47e81f
oalissonbatista <alisson252525@hotmail.com>
2022-12-12 23:32:42 -0300

    Array em c++

---
+++ Programas/C++/Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int vetor[3] = {5,1,2};
+
+    for (int i = 0; i < sizeof(vetor)/4 ; i++){  
+        cout << vetor[i] << "\n";
+    }
+
+    int matriz[3][3];
+
+    for(int i = 0; i < 3 ; i++){
+        for(int j = 0; j < 3; j++){
+            cin >> matriz[i][j];
+        }
+    }
+
+      for(int i = 0; i < 3 ; i++){
+        for(int j = 0; j < 3; j++){
+            cout << matriz[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+
+
+    return 0;
+}
